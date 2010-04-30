a3449fe0b94d775617d2c3e3ee096d76b92fba6e
ronen <ronen@Pluto.(none)>
2010-05-01 02:32:30 +0300

    removing binary stuff

--- packaging/debian/couchfuse/usr/share/couchfuse/native/fuse_callback.h
+++
@@ -1,8 +0,0 @@
-#ifndef _FUSE_CALLBACK_H_
-#define _FUSE_CALLBACK_H_
-
-#include "javafs.h"
-
-extern struct fuse_operations javafs_oper;
-
-#endif
--- packaging/debian/couchfuse/usr/share/couchfuse/native/native_impl.h
+++
@@ -1,8 +0,0 @@
-#ifndef _NATIVE_IMPL_H_
-#define _NATIVE_IMPL_H_
-
-#include "javafs.h"
-
-int RegisterNativeMethods(JNIEnv *env);
-
-#endif
