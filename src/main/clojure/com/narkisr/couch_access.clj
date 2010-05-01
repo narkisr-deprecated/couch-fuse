9f7fc52795cefad4cea75d48b7d4c6b26e7dd99e
ronen <ronen@Pluto.(none)>
2010-05-01 18:44:49 +0300

    added validations into the mix

--- src/main/clojure/com/narkisr/couch_access.clj
+++ src/main/clojure/com/narkisr/couch_access.clj
@@ -5,8 +5,7 @@
     (com.narkisr fs-logic)
     (couchdb (client :only [document-list document-get view-get]))
     (clojure.contrib (str-utils2 :only [contains?]) error-kit (def :only [defn-memo]))
-    (clojure.contrib.json read write)
-    ))
+    (clojure.contrib.json read write)))
 
 (def *host* "http://127.0.0.1:5984/")
 (def *db* "fuse")
@@ -34,7 +33,12 @@
   "Fetches file size in bytes using http HEAD, note that size is + 1 more than the actual content size."
   (-> (str *host* *db* "/" (:name file)) resourcefully/head (get-in [:headers :content-length]) first Integer/parseInt (- 1)))
 
+(defn db-exists? [host db]
+ (try (resourcefully/get (str host db))
+   (catch java.io.IOException e nil)))
+
+(db-exists? *host* "fuse")
 ;(fetch-size {:name "e71a88687bef0c9e4db04f8191272638"})
 ;(alength (-> (str *host* *db* "/" "e71a88687bef0c9e4db04f8191272638") resourcefully/get :body-seq first (. getBytes)))
 
-(map  #(str (-> (str *host* *db* "/" %) resourcefully/get :body-seq first (. length)) " vs " (fetch-size {:name %}) ) (all-ids))
+;(map  #(str (-> (str *host* *db* "/" %) resourcefully/get :body-seq first (. length)) " vs " (fetch-size {:name %}) ) (all-ids))
--- src/main/clojure/com/narkisr/mounter.clj
+++ src/main/clojure/com/narkisr/mounter.clj
@@ -1,10 +1,25 @@
 (ns com.narkisr.mounter
   (:gen-class)
   (:import fuse.FuseMount org.apache.commons.logging.LogFactory)
-  (:use com.narkisr.couch-fs))
+  (:use com.narkisr.couch-fs com.narkisr.couch-access clojure.contrib.command-line))
 
+(defn valid? [cond message]
+  (let [log (LogFactory/getLog (class com.narkisr.mounter))]
+    (if cond
+      (do (. log error message) (System/exit 1)))))
 
-(defn -main [path]
-  (FuseMount/mount
-    (into-array  [path "-f"])
-    (com.narkisr.couch-fuse.) (LogFactory/getLog (class com.narkisr.mounter))))
+(defn validate [host path db]
+  (doseq [[cond error] [[(some empty? [db path]) "db and path are mandatory."] [(not (db-exists? host db)) "db does not exists."]]]
+    (valid? cond error)))
+
+(defn -main [& args]
+  (with-command-line args "Couchdb fuse filesystem 0.1"
+    [[host "Couchdb host name" "http://127.0.0.1:5984/"]
+     [db "Couchdb db name"]
+     [path "Mount path on local filesystem"] remaining]
+    (validate host db path)
+    (alter-var-root #'*host* (fn [_] (identity host)))
+    (alter-var-root #'*db* (fn [_] (identity db)))
+    (FuseMount/mount
+      (into-array [path "-f"])
+      (com.narkisr.couch-fuse.) (LogFactory/getLog (class com.narkisr.mounter)))))
