b855da6c3f6d34748ca4b3c37169ce0d15705d7e
ronen <ronen@Pluto.(none)>
2010-05-01 19:38:05 +0300

    using /var/log to store the log & added mount exit on validations failure

--- mount.sh
+++ mount.sh
@@ -1,1 +1,11 @@
-java -Djava.library.path=/usr/lib:/usr/lib/jvm/java-6-sun/jre/lib/i386/server:/usr/share/couchfuse/native -jar target/couch-fuse-0.1-jar-with-dependencies.jar $@
+#!/bin/sh
+java -Djava.library.path=/usr/lib:/usr/lib/jvm/java-6-sun/jre/lib/i386/server:/usr/share/couchfuse/native -jar target/couch-fuse-0.1-jar-with-dependencies.jar $@ -run-valid true
+
+exitValue=$? 
+
+if [ $exitValue != 0 ] 
+then 
+	exit $exitValue 
+fi 
+
+nohup java -Djava.library.path=/usr/lib:/usr/lib/jvm/java-6-sun/jre/lib/i386/server:/usr/share/couchfuse/native -jar target/couch-fuse-0.1-jar-with-dependencies.jar $@  > /var/log/couchfuse.log &
--- src/main/clojure/com/narkisr/mounter.clj
+++ src/main/clojure/com/narkisr/mounter.clj
@@ -4,22 +4,19 @@
   (:use com.narkisr.couch-fs com.narkisr.couch-access clojure.contrib.command-line))
 
 (defn valid? [cond message]
-  (let [log (LogFactory/getLog (class com.narkisr.mounter))]
-    (if cond
-      (do (. log error message) (System/exit 1)))))
+  (if cond (do (println message) (System/exit 1))))
 
-(defn validate [host path db]
+(defn validate [host db path]
   (doseq [[cond error] [[(some empty? [db path]) "db and path are mandatory."] [(not (db-exists? host db)) "db does not exists."]]]
     (valid? cond error)))
 
+(defn mount [host db path]
+  (alter-var-root #'*host* (fn [_] (identity host)))
+  (alter-var-root #'*db* (fn [_] (identity db)))
+  (FuseMount/mount (into-array [path "-f"]) (com.narkisr.couch-fuse.) (LogFactory/getLog (class com.narkisr.mounter))))
+
 (defn -main [& args]
   (with-command-line args "Couchdb fuse filesystem 0.1"
-    [[host "Couchdb host name" "http://127.0.0.1:5984/"]
-     [db "Couchdb db name"]
-     [path "Mount path on local filesystem"] remaining]
-    (validate host db path)
-    (alter-var-root #'*host* (fn [_] (identity host)))
-    (alter-var-root #'*db* (fn [_] (identity db)))
-    (FuseMount/mount
-      (into-array [path "-f"])
-      (com.narkisr.couch-fuse.) (LogFactory/getLog (class com.narkisr.mounter)))))
+    [[run-valid? "runs validation only" false] [host "Couchdb host name" "http://127.0.0.1:5984/"]
+     [db "Couchdb db name"] [path "Mount path on local filesystem"] remaining]
+    (if run-valid? (validate host db path) (mount host db path))))
