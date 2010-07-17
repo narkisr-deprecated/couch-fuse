5ceb7188901c8ea0be6cfb0ab259c59b2ea8024f
narkisr <narkisr.dev@gmail.com>
2010-07-17 22:35:34 +0300

    explicit import

--- src/main/clojure/com/narkisr/couchfs/file_update.clj
+++ src/main/clojure/com/narkisr/couchfs/file_update.clj
@@ -1,13 +1,16 @@
 (ns com.narkisr.couchfs.file-update
-  (:use (com.narkisr.couchfs couch-access)
-     (com.narkisr fs-logic)))
+  (:use 
+     (com.narkisr.couchfs 
+          (couch-access :only [couch-attachment-content add-attachment update-document get-document]))
+     (com.narkisr (fs-logic :only [update update-atime]))))
 
 (defn update-rev-and-time [path rev-map]
   (update path :_rev (rev-map :_rev))
   (update-atime path (System/currentTimeMillis)))
 
 (defn use-lastest-rev [path id contents]
-  (update-rev-and-time path (update-document id (assoc contents :_rev ((get-document id) :_rev)))))
+  (update-rev-and-time path 
+    (update-document id (assoc contents :_rev ((get-document id) :_rev)))))
 
 (defn update-attachment [path couch-id file-name contents ]
   (add-attachment couch-id file-name contents "text/plain")
