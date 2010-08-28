b1b1cb3c06d4e12d41997ee9330e8a0c8f0f95f2
narkisr <narkisr.dev@gmail.com>
2010-08-29 02:19:08 +0300

    starting to implement protocols

---
+++ src/main/clojure/com/narkisr/protocols.clj
@@ -0,0 +1,25 @@
+(ns com.narkisr.protocols
+  (:use (clojure.contrib (def :only [defmacro-]))))
+
+(defn with-type [type x]
+  (with-meta x {:type type}))
+
+(defn into-syms [keys]
+  (vec (map #(. % sym) (into [:name :mode :xattrs :lastmod] keys))))
+
+(defmacro fstype [name & keys]
+  `(defrecord ~name ~(into-syms keys)))
+
+(fstype Directory :files)
+(fstype Meta :file)
+(fstype File :content :size)
+
+(defprotocol Handle
+             (delete [this])
+             (create [this]))
+
+(defprotocol FsElement
+             )
+
+(defprotocol FsContainer
+             )
