3020a0002ccf1a5c81c94fe3057755d99ea16e6c
narkisr <narkisr.dev@gmail.com>
2010-09-17 19:01:19 +0200

    printing failing form on pre condition failure

--- src/main/clojure/com/narkisr/common_fs.clj
+++ src/main/clojure/com/narkisr/common_fs.clj
@@ -1,6 +1,6 @@
 (ns com.narkisr.common-fs
   (:import org.apache.commons.logging.LogFactory)
-  (:use (clojure.contrib (seq-utils :only [find-first] ))))
+  (:use (clojure.contrib (seq-utils :only [find-first]) (pprint))))
 
 (defn log-info [this text]
   (. (LogFactory/getLog (class this)) info text))
@@ -8,8 +8,11 @@
 (defn log-warn [this text]
   (. (LogFactory/getLog (class this)) warn text))
 
+(defn forms [pre]
+  (map (fn [[k v]] k) pre))
+
 (defn first-error [pre]
-   (find-first (fn [[k v]] (not k)) pre))
+  (find-first (fn [[[cond errno] form]] (not cond)) pre))
 
 (defmacro def-fs-fn
   ([name args] `(def-fs-fn ~name ~args {} (identity 0)))
@@ -17,7 +20,11 @@
   ([name args pre body]
     (let [fn-name (clojure.lang.Symbol/intern (str "fs-" name))]
       `(defn ~fn-name ~(into ['this] args)
-         (let [error# (first-error (:pre ~pre))]
-          (if-not error# 
-           (do (log-info 'this (str "calling - > " ~fn-name)) ~body (identity 0)) 
-            (or (second error#) (:default ~pre) )))))))
+         (let [forms# '~(-> pre :pre forms) error# (first-error (map vector (:pre ~pre) forms#))]
+           (if-not error# 
+            (do (log-info 'this (str "calling - > " ~fn-name)) ~body (identity 0)) 
+            (let [cond-form# (error# 1) code# (or (get-in error# [0 1]) (:default ~pre))] 
+              (log-warn 'this (str "pre condition " cond-form# " failed with code " code#)) code#)))))))
+
+
+#_(pprint (macroexpand-1 '(def-fs-fn bla [path mode] {:pre [[(nil? path) -1] [(pos? mode) -2] ]} (println "done"))))
