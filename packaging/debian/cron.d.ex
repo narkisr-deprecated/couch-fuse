db38312873b3d252859cdb3a7c2432fcd90cbe62
narkisr <narkisr@gmail.com>
2012-04-22 02:33:07 +0300

    clearing old deb sandbox

--- packaging/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the couchfuse package
-#
-0 4	* * *	root	[ -x /usr/bin/couchfuse_maintenance ] && /usr/bin/couchfuse_maintenance
--- packaging/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(couchfuse):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="couchfuse" command="/usr/bin/couchfuse"
