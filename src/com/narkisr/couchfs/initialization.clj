16a93eea7f2b6a60db2a87a0ad94c166463bbb15
narkisr <narkisr@gmail.com>
2012-04-29 02:22:38 +0300

    handling clojure.couchdb exception in case that the document is missing

--- src/com/narkisr/couchfs/initialization.clj
+++ src/com/narkisr/couchfs/initialization.clj
@@ -2,9 +2,17 @@
   (:import com.narkisr.protocols.Directory 
            com.narkisr.protocols.Root
            com.narkisr.protocols.MetaFolder
-           com.narkisr.protocols.File)
-  (:require [com.narkisr.couchfs.couch-access :as couch])
-  (:use (com.narkisr (file-info :only [fname parent-path combine parent-name file-path un-hide hide]) (fs-logic ))))
+           com.narkisr.protocols.File
+           couchdb.errors.DocumentNotFound)
+  (:require 
+    [com.narkisr.couchfs.couch-access :as couch]
+     [couchdb.errors] 
+    )
+  (:use 
+    [slingshot.slingshot :only [try+]]
+    (com.narkisr 
+      (file-info :only [fname parent-path combine parent-name file-path un-hide hide]) 
+      (fs-logic))))
 
 (defn- inner-file [file-name desc mime content-fn size-fn couch-id ]
   (File. file-name 0644 [:description desc :mimetype mime :couch-id couch-id] (/ (System/currentTimeMillis) 1000) content-fn size-fn))
@@ -15,12 +23,16 @@
 
 (defn attachment [couch-id file-name details]
   (with-xattr [:attachment true]
-              (inner-file file-name  "A Couchdb document attachment" (details :content_type) (couch/couch-attachment-content couch-id file-name) #(details :length) couch-id) ))
+    (inner-file file-name  "A Couchdb document attachment" (details :content_type) (couch/couch-attachment-content couch-id file-name) #(details :length) couch-id) ))
+
+(defn safe-attaments [couch-id]
+  (try+ (couch/attachments couch-id)
+        (catch DocumentNotFound _ [])))
 
 (defn- attachments [couch-id]
   (reduce 
     (fn [res [file-name details]] 
-      (assoc res file-name (attachment couch-id file-name details))) {} (couch/attachments couch-id)))
+      (assoc res file-name (attachment couch-id file-name details))) {} (safe-attaments couch-id)))
 
 (defn meta-folder [couch-id hidden]
   (MetaFolder. hidden 0444 [:description "Couch meta folder"] (/ (System/currentTimeMillis) 1000) (json-file couch-id)))
